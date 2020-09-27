5f9886c56aa16098d961546ca1e75a5d05bc6fcc
Galileo Galilei <yolan.honore.rouge@gmail.com>
2020-09-27 23:32:15 +0200

    make more like kedro_mlflow

--- setup.py
+++ setup.py
@@ -1,8 +1,12 @@
+import re
 from setuptools import setup, find_packages
+
+with open("gh_actions_playground/__init__.py", encoding="utf-8") as file_handler:
+    version = re.search(r'__version__ *= *["\']([^"\']+)', file_handler.read()).group(1)
 
 setup(
     name='gh_actions_playground',
-    version='0.5.8',
+    version=version,
     author='Author Name',
     author_email='author@gmail.com',
     description='Tests for CI',
